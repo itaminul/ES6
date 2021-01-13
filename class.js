c46849d9bba1a64eedeba48dd7b0aa1726759d5a
itaminul <itaminul@gmail.com>
2021-01-13 23:16:18 +0600

    class

---
+++ class.js
@@ -0,0 +1,19 @@
+// Class are a template that use for creating object
+class Progrmmer{
+    constructor(type) {
+         this.type = type;
+    }
+
+    javascript() {
+        console.log(this.type); //return I am a javascript progrmmer
+    }
+
+    php(phpType) {
+        console.log(`I am a ${phpType} programmer`) // return I am a PHP programmer
+
+    }
+}   
+
+let obj = new Progrmmer('I am a javascript progrmmer');
+obj.javascript();
+obj.php('PHP');
